4738a1bd6ad2176eb152b0f730a9ec06772c82a7
Emperor <mttmallet@gmail.com>
2026-02-13 16:10:24 +0100

    des trucs

--- Main.py
+++ Main.py
@@ -3,16 +3,23 @@
 # Example file showing a basic pygame "game loop"
 from Objects import *
 import pygame
+from pygame import Vector2
 
 # pygame setup
 pygame.init()
+pygame.display.set_caption("Pypy")
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
 
-player = Player(pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2))
+player = Player(
+    Vector2(screen.get_width() / 2, screen.get_height() / 2), 
+    Vector2(50, 230),
+    r"player.png",
+    speed=600
+    )
 
 
 while running:
@@ -23,19 +30,19 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill(pygame.Color(52, 168, 235))
 
     # RENDER YOUR GAME HERE
     for obj in Object.instances:
         obj.Update(dt)
         obj.Render(screen)
 
-    # flip() the display to put your work on screen
+
+    # update the display to put your work on screen
     pygame.display.flip()
 
     # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
+    # dt is delta time in seconds since last frame, used for framerate-independent physics.
     dt = clock.tick(60) / 1000
 
 pygame.quit()
--- Objects.py
+++ Objects.py
@@ -1,4 +1,6 @@
 import pygame
+from pygame import Vector2
+import os
 
 class Object :
     instances: list["Object"] = []
@@ -15,25 +17,45 @@ class Object :
     def Destroy(self):
         if self in Object.instances:
             Object.instances.remove(self)
+            del self
 
 class Player (Object):
-    def __init__(self, position: pygame.Vector2):
+    def __init__(self, position: pygame.Vector2, size: pygame.Vector2, sprite:str, speed: float = 300):
         self.position = position
+        self.speed = speed
+        self.size = size
+
+        if isinstance(sprite, str):
+            if not os.path.exists(sprite):
+                raise Exception(f"Image not found: {sprite}", isFatal=True)
+            else:
+                img = pygame.image.load(sprite).convert_alpha()
+                self.sprite: pygame.Surface = img
+        elif isinstance(sprite, pygame.Surface):
+            self.sprite: pygame.Surface = sprite
+        else:
+            raise Exception(f"Unsupported image type: {type(image)}", isFatal=True)
 
         super().__init__()
 
-    def Render(self, screen):
-        pygame.draw.circle(screen, "red", self.position, 40)
+    def Render(self, screen: pygame.surface.Surface):
+        surf = pygame.transform.scale(self.sprite, (int(self.size.x), int(self.size.y)))
+
+        world_pos = Vector2(self.position.x - self.size.x/2, self.position.y - self.size.y/2)
+
+        screen.blit(surf, world_pos)
 
     def Update(self, dt):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_z]:
-            self.position.y -= 300 * dt
+            self.position.y -= self.speed * dt
         if keys[pygame.K_s]:
-            self.position.y += 300 * dt
+            self.position.y += self.speed * dt
         if keys[pygame.K_q]:
-            self.position.x -= 300 * dt
+            self.position.x -= self.speed * dt
         if keys[pygame.K_d]:
-            self.position.x += 300 * dt
+            self.position.x += self.speed * dt
+        if keys[pygame.K_t]:
+            self.Destroy()
 
     
